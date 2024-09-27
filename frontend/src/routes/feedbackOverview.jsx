7db1a9c32a255f5a4672bd289e171b63621a5a1e
stoff <christoph.tiger@hotmail.com>
2024-09-27 17:33:52 +0200

    reflect added feedback directly in UI (so it can be modified without reloading the page first)

--- frontend/src/routes/feedbackOverview.jsx
+++ frontend/src/routes/feedbackOverview.jsx
@@ -1,50 +1,45 @@
-import React, { useContext } from "react";
+import React, {useContext} from "react";
 import Feedbacks from "../components/feedbacks";
-import { useMutation } from "@apollo/client";
-import { ADD_COMMENT, ADD_FEEDBACK } from "../graphql/graphs";
-import { Navigate } from "react-router-dom";
+import {useMutation} from "@apollo/client";
+import {ADD_COMMENT, ADD_FEEDBACK} from "../graphql/graphs";
+import {Navigate} from "react-router-dom";
 import UserIcons from "../components/common/icons/user";
-import { schemaFeedback } from "../configs/config.schema";
-import { feedbackInputs } from "../configs/config.inputs";
+import {schemaFeedback} from "../configs/config.schema";
+import {feedbackInputs} from "../configs/config.inputs";
 import Form from "../components/common/form";
-import { getCurrentUser } from "../services/authService";
+import {getCurrentUser} from "../services/authService";
 import FeedbacksContext from "../context/feedbacksContext";
 
 function Home() {
-  const user = getCurrentUser();
-  const [feedbacks, setFeedbacks] = useContext(FeedbacksContext);
-  const [addFeedback, { loading, error, data }] = useMutation(ADD_FEEDBACK);
+    const user = getCurrentUser();
+    const [feedbacks, setFeedbacks] = useContext(FeedbacksContext);
+    const [addFeedback, {loading, error, data}] = useMutation(ADD_FEEDBACK);
 
-  const doSubmit = (newData) => {
-    const allData = [...feedbacks];
-    const addNewData = {
-      author: { id: user.id, username: user.username },
-      comments: [],
-      likes: [],
-      ...newData,
+    const doSubmit = async (newData) => {
+        const {content, title} = newData;
+        const userId = user.id;
+
+        const { data: { createFeedback } } = await addFeedback({variables: {userId, title, content}})
+
+        setFeedbacks([ {
+            id: createFeedback.documentId,
+            author: { id: createFeedback.author.documentId, username: user.username },
+            comments: [],
+            likes: [],
+            ...newData,
+        }, ...feedbacks])
     };
-    allData.unshift(addNewData);
-    setFeedbacks(allData);
 
-    console.log(user)
-    const { content, title } = newData;
-    const userId = user.id;
-
-    addFeedback({ variables: { userId, title, content } });
-  };
-
-  return (
-    <React.Fragment>
-      {!user && <Navigate to="/login" />}
-      <Form
-        schemaObj={schemaFeedback}
-        doSubmit={doSubmit}
-        inputFields={feedbackInputs}
-        iconSize="large"
-      />
-      <Feedbacks loading={loading} />
-    </React.Fragment>
-  );
+    return (<React.Fragment>
+            {!user && <Navigate to="/login"/>}
+            <Form
+                schemaObj={schemaFeedback}
+                doSubmit={doSubmit}
+                inputFields={feedbackInputs}
+                iconSize="large"
+            />
+            <Feedbacks loading={loading}/>
+        </React.Fragment>);
 }
 
 export default Home;
